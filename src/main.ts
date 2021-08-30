058edde55c95ca92cfa282ec1ecc771e544237a6
Sopruk <progerreact@gmail.com>
2021-08-30 22:44:21 +0300

    added User entityes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-
+  const config = new DocumentBuilder()
+    .setTitle('продвинутий Backend')
+    .setDescription('Rest API')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/swagger', app, document);
   await app.listen(PORT, () => {
     console.log(`Server has been started on PORT=${PORT}`);
   });
---
+++ src/users/users.model.ts
@@ -0,0 +1,30 @@
+import { Column, Model, Table } from 'sequelize-typescript';
+import { DataTypes } from 'sequelize';
+
+interface UserCreationAttrs {
+  email: string;
+  password: string;
+}
+
+@Table({ tableName: 'users' })
+export class User extends Model<User, UserCreationAttrs> {
+  @Column({
+    type: DataTypes.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+
+  @Column({ type: DataTypes.STRING, unique: true, allowNull: false })
+  email: string;
+
+  @Column({ type: DataTypes.STRING, allowNull: false })
+  password: string;
+
+  @Column({ type: DataTypes.BOOLEAN, defaultValue: false })
+  banned: boolean;
+
+  @Column({ type: DataTypes.STRING, allowNull: true })
+  banReason: string;
+}
