0eedb82a1da2538994034b735b824c75cc976ebd
Sopruk <progerreact@gmail.com>
2021-09-15 00:23:49 +0300

    added method createFile to FileService

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@nestjs/common';
-
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as uuid from 'uuid';
 @Injectable()
 export class FilesService {
-    async createFile(file){
-
+  async createFile(file): Promise<string> {
+    try {
+      const fileName = uuid.v4() + 'jpeg';
+      const filePath = path.resolve(__dirname, '..', 'static');
+      if (!fs.existsSync(filePath)) {
+        fs.mkdirSync(filePath, { recursive: true });
+      }
+      fs.writeFileSync(path.join(filePath, fileName), file.buffer);
+      return fileName;
+    } catch (e) {
+      throw new HttpException(
+        'Hapened Exception during writing the file!',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
+  }
 }
