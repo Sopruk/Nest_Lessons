e71643674a21b2beb0e542b67c30c588104d91b3
Sopruk <progerreact@gmail.com>
2021-09-15 00:32:19 +0300

    added decorators to post controller

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -4,7 +4,9 @@ import {
   Injectable,
   Post,
   UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { CreatePostDto } from './dto/create-post.dto';
 import { PostsService } from './posts.service';
 
@@ -13,7 +15,8 @@ import { PostsService } from './posts.service';
 export class PostsController {
   constructor(private postService: PostsService) {}
   @Post()
+  @UseInterceptors(FileInterceptor('image'))
   createPost(@Body() dto: CreatePostDto, @UploadedFile() image: any) {
-    this.postService.create(dto, image);
+    return this.postService.create(dto, image);
   }
 }
